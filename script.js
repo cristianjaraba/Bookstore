24976cf652823ec8e50de75272994a0754e36aca
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-06-03 16:58:31 +0200

    feat: add like/dislike toggle with dynamic counter update

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const booksWrapperRef = document.getElementById('books-wrapper');
 
-// const likeBtnRef = document.getElementById('heart-icon');
-
 function renderBooks() {
     booksWrapperRef.innerHTML = "";
     for (let bookIndex = 0; bookIndex < books.length; bookIndex++) {
         booksWrapperRef.innerHTML += getBookHTML(bookIndex);
+        if (books[bookIndex].liked) {
+            const likeBtnRef = document.getElementById('heart-icon' + bookIndex);
+            likeBtnRef.classList.add('liked');
+        }
         const commentsTableRef = document.getElementById('comments-table' + bookIndex);
         for (let commentIndex = 0; commentIndex < books[bookIndex].comments.length; commentIndex++) {
             commentsTableRef.innerHTML += getCommentHTML(bookIndex, commentIndex);
@@ -13,6 +15,11 @@ function renderBooks() {
     }
 }
 
-// function toggleLikeAndLiked() {
-//     likeBtnRef.classList.toggle('liked');
-// }
+function likeDislike(i) {
+    const likeBtnRef = document.getElementById('heart-icon' + i);
+    const likeDisplayerRef = document.getElementById('like-displayer' + i);
+    if (likeBtnRef.classList.toggle('liked')) {
+        likeDisplayerRef.innerHTML = parseInt(likeDisplayerRef.innerHTML) + 1;
+    }
+    else likeDisplayerRef.innerHTML = parseInt(likeDisplayerRef.innerHTML) - 1;
+}

ce6097af0b617b3bc48568857731c1df53f4f215
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-06-03 14:56:22 +0200

    feat: generate book and comment cards using template functions

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const likeBtnRef = document.getElementById('heart-icon');
+const booksWrapperRef = document.getElementById('books-wrapper');
 
-function toggleLikeAndLiked() {
-    likeBtnRef.classList.toggle('liked');
+// const likeBtnRef = document.getElementById('heart-icon');
+
+function renderBooks() {
+    booksWrapperRef.innerHTML = "";
+    for (let bookIndex = 0; bookIndex < books.length; bookIndex++) {
+        booksWrapperRef.innerHTML += getBookHTML(bookIndex);
+        const commentsTableRef = document.getElementById('comments-table' + bookIndex);
+        for (let commentIndex = 0; commentIndex < books[bookIndex].comments.length; commentIndex++) {
+            commentsTableRef.innerHTML += getCommentHTML(bookIndex, commentIndex);
+        }
+    }
 }
+
+// function toggleLikeAndLiked() {
+//     likeBtnRef.classList.toggle('liked');
+// }

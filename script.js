1c925ae2bd2a7cbcd8c3774cb61cfc9860be9953
Cristian Jaraba <cristianjaraba@hotmail.com>
2026-06-04 11:01:39 +0200

    feat: add scale animation to like button when toggled

--- script.js
+++ script.js
@@ -19,20 +19,25 @@ function likeDislike(i) {
     const likeBtnRef = document.getElementById('heart-icon' + i);
     const likeDisplayerRef = document.getElementById('like-displayer' + i);
     if (likeBtnRef.classList.toggle('liked')) {
+        // Quelle: Ulrike Hässler, JavaScript Schnelleinstieg, Kapitel 10.3.5 „CSS‑Stile ändern“
+        // CSS-Animation per JavaScript setzen
+        likeBtnRef.style.animationName = 'myAnimation';
+        likeBtnRef.style.animationDuration = '2s';
         likeDisplayerRef.innerHTML = parseInt(likeDisplayerRef.innerHTML) + 1;
     }
     else likeDisplayerRef.innerHTML = parseInt(likeDisplayerRef.innerHTML) - 1;
 }
 
 function addComment(i) {
-    const inputRef = document.getElementById('input' + i)
-    const newComment = inputRef.value;
+    const inputRef = document.getElementById('input' + i);
     const tableRef = document.getElementById('comments-table' + i);
 
     tableRef.innerHTML += `
 <tr>
-    <th>[banana123]</th>
-    <td>:  ${newComment}</td>
-</tr>`;
+    <th>[anonym]</th>
+    <td>:  ${inputRef.value}</td>
+</tr>
+`;
     inputRef.value = "";
 }
+
